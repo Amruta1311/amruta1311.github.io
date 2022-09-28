4413563711fa6b2c1fc74e6d04df8f5f90431855
Amruta1311 <amruta13mulay11@gmail.com>
2022-09-28 09:00:09 -0700

    Deploy v

---
+++ precache-manifest.ab8b9b15fdf867c01d9a0eeaa26d5edb.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d6f0864a70e6b2528263fd797b55e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c83c78c1c73ec2e6e4d3",
+    "url": "/static/css/3.7822d34d.chunk.css"
+  },
+  {
+    "revision": "54cd90992f056854f6dc",
+    "url": "/static/css/4.b375e3ca.chunk.css"
+  },
+  {
+    "revision": "a7d750ed887b8088adee",
+    "url": "/static/css/main.8cb66128.chunk.css"
+  },
+  {
+    "revision": "17a005f9d33c56648af8",
+    "url": "/static/js/2.681c89eb.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.681c89eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c83c78c1c73ec2e6e4d3",
+    "url": "/static/js/3.8a93dbd9.chunk.js"
+  },
+  {
+    "revision": "54cd90992f056854f6dc",
+    "url": "/static/js/4.09e78f1c.chunk.js"
+  },
+  {
+    "revision": "a7d750ed887b8088adee",
+    "url": "/static/js/main.92a277fc.chunk.js"
+  },
+  {
+    "revision": "97dcfdb9755deac54844",
+    "url": "/static/js/runtime-main.3c7287b5.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "2238b99bc0bf9d5166eadd849919a9e4",
+    "url": "/static/media/DPS.2238b99b.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "91641e3b456269b48869a7e5af978c61",
+    "url": "/static/media/SVNIT.91641e3b.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e0e58a5a166a40545fc7d03cb24c11fc",
+    "url": "/static/media/jpmc.e0e58a5a.jpeg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "6d04c6cc39528179b66af85693bcee0c",
+    "url": "/static/media/nitw.6d04c6cc.jpeg"
+  },
+  {
+    "revision": "995aee343ff8fc1387d33a67cc36be90",
+    "url": "/static/media/taylor&francis.995aee34.png"
+  },
+  {
+    "revision": "9e8aca440a9ed65e0dcca642fce6fc91",
+    "url": "/static/media/usc.9e8aca44.jpeg"
+  }
+]);
